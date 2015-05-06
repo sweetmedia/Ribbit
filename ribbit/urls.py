16583c8e1326e8482ac320220e94a7fb710e0b41
Juan Perez <juanto@wearesweetmedia.com>
2015-05-06 02:14:59 -0400

    Implemented User Login and Sign Up

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'ribbit_app.views.index'), # root
+    url(r'^login$', 'ribbit_app.views.login_view'), # login
+    url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
+    url(r'^signup$', 'ribbit_app.views.signup'), # signup
 )
